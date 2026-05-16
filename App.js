4040f3634ac10314528c316a36ca819cde11b576
mdkhalidhossain <zikaiwen02@gmail.com>
2026-05-16 16:43:40 +0600

    added

--- App.js
+++ App.js
@@ -1,5 +1,18 @@
+import React from 'react'
 import AppNavigator from './src/navigation/AppNavigator'
 
+import { useFonts } from 'expo-font'
+
+import Ionicons from '@expo/vector-icons/Ionicons'
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    ...Ionicons.font,
+  })
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return <AppNavigator />
 }
